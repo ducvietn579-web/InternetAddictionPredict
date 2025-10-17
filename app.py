fcfa43d63db55718426917016d889ec278a6c032
ducvietn579-web <ducvietn579@gmail.com>
2025-10-17 21:59:22 +0700

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 st.title("Dự đoán Mức độ Nghiện Internet")
 
 # Tải mô hình đã huấn luyện
-model = pickle.load(open("GDmodel_enc.pkl", "rb"))
+model = pickle.load(open("GDmodel_enc.pkl"))
 
 # Giao diện nhập liệu
 st.header("Nhập thông tin người dùng:")
