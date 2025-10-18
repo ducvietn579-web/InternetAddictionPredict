2156e045e75a7669418905422313d89355aa0d9b
ducvietn579-web <ducvietn579@gmail.com>
2025-10-18 22:53:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,22 +19,20 @@ def main():
             academic = st.selectbox("Trình độ học vấn", ["Graduate", "Undergraduate", "High School"])
             relationship = st.selectbox("Tình trạng mối quan hệ", ["Single", "In a relationship", "Complicated"])
            platform = st.selectbox(
-    "Nền tảng sử dụng nhiều nhất",
-    [
-        "Instagram",
-        "LINE",
-        "VK",
-        "Facebook",
-        "LinkedIn",
-        "WhatsApp",
-        "TikTok",
-        "KakaoTalk",
-        "WeChat",
-        "Twitter",
-        "YouTube",
-        "Snapchat"
-    ]
-)
+                                    "Nền tảng sử dụng nhiều nhất",["Instagram",
+                                                                   "LINE",
+                                                                   "VK",
+                                                                   "Facebook",
+                                                                   "LinkedIn",
+                                                                   "WhatsApp",
+                                                                   "TikTok",
+                                                                   "KakaoTalk",
+                                                                   "WeChat",
+                                                                   "Twitter",
+                                                                   "YouTube",
+                                                                   "Snapchat"
+                                                                ]
+                                                            )
             sleep_hours = st.number_input("Số giờ ngủ mỗi đêm", min_value=0.0, max_value=12.0, value=7.0)
             mental_health = st.slider("Điểm sức khỏe tâm lý (1-10)", 1, 10, 5)
             usage_hours = st.number_input("Số giờ sử dụng Internet mỗi ngày", min_value=0.0, max_value=24.0, value=5.0)
