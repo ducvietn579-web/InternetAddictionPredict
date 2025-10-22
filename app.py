48c8b23cdd22d4aea1df551cd447ee9335ff12dc
ducvietn579-web <ducvietn579@gmail.com>
2025-10-22 16:57:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 
 # --- Load mô hình ---
 try:
-    loaded = joblib.load("GDmodel_enc.rpk")
+    loaded = joblib.load("GDmodel.pkl")
     if isinstance(loaded, tuple) and len(loaded) == 2:
         rf_model, encoder = loaded
         st.success("Đã load thành công mô hình Random Forest và encoder")
